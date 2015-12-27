e39dbe679016de55186b6a5858212e8d52dafcf5
Allan Cutler <iamcutler@icloud.com>
2015-12-27 00:15:30 -0800

    feat(deps): Introduced AngularJS

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -28,6 +28,8 @@ var jsFilesToInject = [
   'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
+  'js/dependencies/angular/angular.min.js',
+  'js/dependencies/angular/angular-ui-router.min.js',
   'js/dependencies/**/*.js',
 
   // All of the rest of your client-side js files
