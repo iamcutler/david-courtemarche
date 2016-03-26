d58c16635e47fa9b163e1e201c651bd17ec4facd
Allan Cutler <iamcutler@icloud.com>
2016-03-26 12:15:29 -0700

    Event to drive media player on selection

--- assets/js/angular/components/MediaPlayer.js
+++ assets/js/angular/components/MediaPlayer.js
@@ -1,23 +1,29 @@
 'use strict';
 
 angular.module('DavidCourtemarche.directives')
+  .controller('MediaPlayerController', MediaPlayerController)
   .directive('mediaPlayer', MediaPlayer);
 
 function MediaPlayer() {
   return {
     restrict: 'E',
     replace: true,
-    scope: {
-      title: '@'
-    },
+    scope: {},
+    controller: 'MediaPlayerController',
+    controllerAs: 'MediaPlayerCtrl',
     template: `
       <div class="media-player">
-        <div class="player">
+        <div class="player"></div>
 
-        </div>
-
-        <span class="title">{{ title }}</span>
+        <span class="title">{{ MediaPlayerCtrl.video.name }}</span>
       </div>
     `
   };
+}
+
+function MediaPlayerController($rootScope) {
+  // Listen for video changes and display
+  $rootScope.$on('video:selected', (event, video) => {
+    this.video = video;
+  });
 }
--- assets/js/angular/components/VideoThumb.js
+++ assets/js/angular/components/VideoThumb.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('DavidCourtemarche.directives')
+  .controller('VideoThumbController', videoThumbController)
   .directive('videoThumb', VideoThumb);
 
 function VideoThumb() {
@@ -8,18 +9,29 @@ function VideoThumb() {
     restrict: 'E',
     replace: true,
     scope: {
-      title: '='
+      video: '='
     },
+    controller: 'VideoThumbController',
+    controllerAs: 'VideoThumbCtrl',
+    bindToController: true,
     template: `
-      <a href="">
+      <a href="" ng-click="VideoThumbCtrl.selectVideo()">
         <figure>
           <div class="img">
             <img src="https://johnero.files.wordpress.com/2012/09/usher-dive-082412.jpg">
           </div>
 
-          <figcaption>{{ title }}</figcaption>
+          <figcaption>{{ VideoThumbCtrl.video.name }}</figcaption>
         </figure>
       </a>
     `
   };
+}
+
+function videoThumbController($rootScope) {
+  this.selectVideo = selectVideo;
+
+  function selectVideo() {
+    $rootScope.$broadcast('video:selected', this.video);
+  }
 }
